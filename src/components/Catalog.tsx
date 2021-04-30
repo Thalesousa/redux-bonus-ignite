af9093bf228997a3fbfe93d45f7114fd343ec2ae
thalesousa <thalestjsb@gmail.com>
2021-04-30 16:05:35 -0300

    Abstraindo item do catálogo

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,10 @@
-import { useCallback, useEffect, useState } from "react"
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react"
 
 import { IProduct } from "../store/modules/cart/types"
-import { addProductToCart } from "../store/modules/cart/actions";
 import api from "../services/api";
+import {CatalogItem} from "./CatalogItem";
 
 export function Catalog() {
-  const dispatch = useDispatch();
   const [catalog, setCatolog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -15,25 +13,14 @@ export function Catalog() {
     })
   }, []);
 
-  const handleAddProductToCart = useCallback((product: IProduct) => {
-    dispatch(addProductToCart(product))
-  }, [dispatch])
+
 
   return (
     <>
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> {"  "}
-          <button 
-            type="button"
-            onClick={() => handleAddProductToCart(product)}
-          >
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </>
   )
