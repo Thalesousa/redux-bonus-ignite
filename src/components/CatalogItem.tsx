5da27363adab6d171eadd7a3d070da2735481985
thalesousa <thalestjsb@gmail.com>
2021-05-02 21:02:08 -0300

    Sinalizando falta de estoque

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import { IState } from '../store'
 
 interface CatalogItemProps {
   product: IProduct
@@ -9,6 +10,10 @@ interface CatalogItemProps {
 
 export function CatalogItem({product}: CatalogItemProps) {
   const dispatch = useDispatch();
+  
+  const hasFailedStockCheck = useSelector<IState, boolean>(state => {
+    return state.cart.failedStockCheck.includes(product.id)
+  })
 
   const handleAddProductToCart = useCallback(() => {
     dispatch(addProductToCartRequest(product))
@@ -24,6 +29,7 @@ export function CatalogItem({product}: CatalogItemProps) {
       >
         Comprar
       </button>
+      { hasFailedStockCheck && <span style={{ color: 'red'}}>Falta de estoque</span>}
     </article>
   )
 }
